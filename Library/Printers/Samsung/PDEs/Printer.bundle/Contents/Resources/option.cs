cb76df8893e0d8695963e19ca58bc609f2231409
Darren Shu <oemunlock@gmail.com>
2023-11-19 10:36:36 -0600

    updated drivers

---
+++ Library/Printers/Samsung/PDEs/Printer.bundle/Contents/Resources/option.cs
@@ -0,0 +1,27 @@
+bResolution = true;
+
+bQuality = true;
+
+bImage = true;
+
+bColor = true;
+
+bDarkness = true;
+
+bTonersave = true;
+
+bReprint = true;
+
+bHighaltitude = true;
+
+bPowersave = true;
+
+bAboutButtons =  true;
+
+bPaper = true;
+
+bJobProof = true;
+
+bBlackOptimization = true;
+
+bPageColorSetting = true;
